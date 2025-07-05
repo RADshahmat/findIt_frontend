fa73d5cf0cca80bc9aa48b717316c122affe8922
baten005 <hrkhan16@cse.pstu.ac.bd>
2025-07-06 05:24:00 +0600

    jnhgghbj

--- src/axios/axiosInstance.jsx
+++ src/axios/axiosInstance.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://93.127.166.229:5000/', 
+  //baseURL: 'http://93.127.166.229:5000/', 
+  baseURL: 'http://localhost:5000/', 
   timeout: 10000, 
   headers: {
     'Content-Type': 'application/json',
